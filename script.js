c657d0fedaa6c7986092e0b8948d890452cb6517
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-21 06:23:37 +0000

    Update DOM with full library on book creation

--- script.js
+++ script.js
@@ -7,42 +7,44 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.hasRead = hasRead;
-        this.container = null;
+        // this.container = null;
     }
 }
 
 function addBookToLibrary(title, author, pages, hasRead) {
     let book = new Book(title, author, pages, hasRead);
     myLibrary.push(book);
-    addBookToDOM(book);
+    updateDOM();
 }
 
-function addBookToDOM(book) {
-    let book_DOM = {
-        container: document.createElement(`div`),
-        title: document.createElement(`h1`),
-        author: document.createElement(`h2`),
-        pages: document.createElement(`p`),
-        hasRead: document.createElement(`p`),
-    }
+function updateDOM() {
+    myLibrary.forEach(e => {
+        let book_DOM = {
+            container: document.createElement(`div`),
+            title: document.createElement(`h1`),
+            author: document.createElement(`h2`),
+            pages: document.createElement(`p`),
+            hasRead: document.createElement(`p`),
+        }
 
-    book_DOM.container.classList.add(`book`);
-    book_DOM.container.setAttribute(`id`, `book`);
+        book_DOM.container.classList.add(`book`);
+        book_DOM.container.setAttribute(`id`, `book`);
 
-    book_DOM.title.textContent = book.title;
-    book_DOM.author.textContent = book.author;
-    book_DOM.pages.textContent = `Pages: ${book.pages}`;
-    book_DOM.hasRead.textContent = book.hasRead ? `Has read` : `Has not read`;
+        book_DOM.title.textContent = e.title;
+        book_DOM.author.textContent = e.author;
+        book_DOM.pages.textContent = `Pages: ${e.pages}`;
+        book_DOM.hasRead.textContent = e.hasRead ? `Has read` : `Has not read`;
 
-    book_DOM.container.appendChild(book_DOM.title);
-    book_DOM.container.appendChild(book_DOM.author);
-    book_DOM.container.appendChild(book_DOM.pages);
-    book_DOM.container.appendChild(book_DOM.hasRead);
+        book_DOM.container.appendChild(book_DOM.title);
+        book_DOM.container.appendChild(book_DOM.author);
+        book_DOM.container.appendChild(book_DOM.pages);
+        book_DOM.container.appendChild(book_DOM.hasRead);
 
 
-    book.container = book_DOM.container;
+        e.container = book_DOM.container;
 
-    container.appendChild(book_DOM.container);
+        container.appendChild(book_DOM.container);
+    })
 }
 
 addBookToLibrary(`Dune`, `Frank Herbert`, 577, true);
